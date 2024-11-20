7fce7deb39ff30d0bd083a8b139b32167bda2c7e
suubinjo <suubinjo@gmail.com>
2024-11-19 20:27:56 -0800

    finished

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+    filtered_fruits = [
+        fruit for fruit in fruits
+        if fruit["name"].startswith("o") and fruit["quantity"] < 3
+    ]
+    return render_template("index.html", fruits=filtered_fruits)
+
+if __name__ == "__main__":
+    app.run(debug=True)
